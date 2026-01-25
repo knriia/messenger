bf0fee540ac556d51c1251a55d839bc2407a93ea
ivan <ialeksandrov@tender.pro>
2026-01-25 19:17:30 +0700

    расширены настройки созданы docker файлы, добавлен gitignore

--- app/di/container.py
+++ app/di/container.py
@@ -1,24 +1,33 @@
-from fastapi import Request
 from dishka import Provider, Scope, provide
-from app.database.session import DatabaseSessionManager, AsyncSession
+from app.database.session import AsyncSession, AsyncEngine
 from app.services.chat import ChatService
 from app.services.connection_manager import ConnectionManager
 from typing import AsyncGenerator
+from app.core.config import Settings
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 class DatabaseProvider(Provider):
     # Scope.APP означает, что он будет создан один раз при запуске приложения.
     @provide(scope=Scope.APP)
-    def get_db_manager(self, request: Request) -> DatabaseSessionManager:
-        db_url = request.app.state.db_url
-        manager = DatabaseSessionManager()
-        manager.init(db_url)
-        return manager
+    def get_settings(self) -> Settings:
+        return Settings()
+
+    @provide(scope=Scope.APP)
+    def get_engine(self, settings: Settings) -> AsyncEngine:
+        return create_async_engine(settings.db_url)
+
+    @provide(scope=Scope.APP)
+    def get_session_factory(self, engine: AsyncEngine) -> async_sessionmaker:
+        return async_sessionmaker(engine, expire_on_commit=False)
 
     # Scope.REQUEST означает, что новая сессия создается для каждого HTTP-запроса/WS-соединения.
     @provide(scope=Scope.REQUEST)
-    async def get_session(self, manager: DatabaseSessionManager) -> AsyncGenerator[AsyncSession, None]:
-        async with manager.get_session() as session:
+    async def get_session(
+            self,
+            session_factory: async_sessionmaker[AsyncSession]
+    ) -> AsyncGenerator[AsyncSession, None]:
+        async with session_factory() as session:
             yield session
 
     @provide(scope=Scope.REQUEST)
